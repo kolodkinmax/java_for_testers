3199b1320465254b47a0f5fcb2514701102539e9
Max Kolodkin <MKolodkin@alfabank.ru>
2025-05-15 19:53:47 +0500

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,53 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+
+    private String browser;
+
+    private Properties properties;
+
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+            if (properties.getProperty("web.windowsize") != null) {
+                String[] windowSize = properties.getProperty("web.windowsize").split(",");
+                driver.manage().window().setSize(new Dimension(Integer.parseInt(windowSize[0]), Integer.parseInt(windowSize[1])));
+            } else {
+                driver.manage().window().maximize();
+            }
+
+        }
+        return driver;
+    }
+
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -0,0 +1,50 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.nio.file.Paths;
+
+public class HelperBase {
+
+    protected final ApplicationManager manager;
+
+    public HelperBase(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        manager.driver().findElement(locator).click();
+    }
+
+    protected void moveTo(By locator) {
+        Actions action = new Actions(manager.driver());
+        action.moveToElement(manager.driver().findElement(locator)).perform();
+    }
+
+    protected void moveTo(WebElement webElement) {
+        Actions action = new Actions(manager.driver());
+        action.moveToElement(webElement).perform();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
+    }
+
+    protected void selectType(By locator, String text) {
+        click(locator);
+        new Select(manager.driver().findElement(locator)).selectByValue(text);
+    }
+
+    protected void attach(By locator, String file) {
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+    }
+
+    protected boolean isElementPresent(By locator) {
+        return manager.driver().findElements(locator).size() > 0;
+    }
+}
